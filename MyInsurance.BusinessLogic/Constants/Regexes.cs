08d53c2299cba817e219f35cdc74dcbcac0e5195
pawel2272 <pawel2272@gmail.com>
2021-01-04 16:59:10 +0100

    regex change

--- MyInsurance.BusinessLogic/Constants/Regexes.cs
+++ MyInsurance.BusinessLogic/Constants/Regexes.cs
@@ -5,12 +5,12 @@ namespace MyInsurance.BusinessLogic.Constants
 {
     public static class Regexes
     {
-        public static readonly Regex NAME_REGEX = new Regex("^[A-ZŻŹĆĄŚĘŁÓŃ]'?[a-zżźćńółęąśA-ZŻŹĆĄŚĘŁÓŃ]+(-[a-zżźćńółęąśA-ZŻŹĆĄŚĘŁÓŃ]+)?$");
-        public static readonly Regex EMAIL_REGEX = new Regex("[a-z0-9_.-]+@[a-z0-9_.-]+\\.\\w{2,4}");
-        public static readonly Regex ZIPCODE_REGEX = new Regex("[0-9]{2}-[0-9]{3}");
-        public static readonly Regex PHONENUMBER_REGEX = new Regex("^(\\+[\\d]{1,3})?(\\(?[0-9]{2}\\))?[0-9]{7,}$");
-        public static readonly Regex PESEL_REGEX = new Regex("^\\d{11}$");
-        public static readonly Regex NIP_REGEX = new Regex("^\\d{10}$");
-        public static readonly Regex CITY_REGEX = new Regex("[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+[\\s]?[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]*");
+        public static readonly Regex NAME_REGEX = new Regex(@"^[A-ZŻŹĆĄŚĘŁÓŃ]'?[a-zżźćńółęąśA-ZŻŹĆĄŚĘŁÓŃ]+(-[a-zżźćńółęąśA-ZŻŹĆĄŚĘŁÓŃ]+)?$");
+        public static readonly Regex EMAIL_REGEX = new Regex(@"[a-z0-9_.-]+@[a-z0-9_.-]+\.\w{2,4}");
+        public static readonly Regex ZIPCODE_REGEX = new Regex(@"[0-9]{2}-[0-9]{3}");
+        public static readonly Regex PHONENUMBER_REGEX = new Regex(@"^(?:\(?\?)?(?:[-\.\(\)\s]*(\d)){9}\)?$");
+        public static readonly Regex PESEL_REGEX = new Regex(@"^\d{11}$");
+        public static readonly Regex NIP_REGEX = new Regex(@"^\d{10}$");
+        public static readonly Regex CITY_REGEX = new Regex(@"[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+[\s]?[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]*");
     }
 }
